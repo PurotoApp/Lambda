d2b9eacebaab73a9362c0fafe930feea8f1c6fa0
Alex <alexandre.olemans@gmail.com>
2022-01-14 00:11:03 +0100

    deps: add @rollup/plugin-json

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
+import json from '@rollup/plugin-json'
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -50,6 +51,11 @@ export default {
 				}
 			})
 		}),
+
+		json({
+			compact: true
+		}),
+
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
 		css({ output: 'bundle.css' }),
